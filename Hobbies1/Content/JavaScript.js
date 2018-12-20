ff517b85b19e9bc148e712b8fb28f27e2254f53c
pierremaxencemazince <pierre-maxence.mazin@hotmail.fr>
2018-12-20 16:13:55 +0100

    Hobbies_v_2

---
+++ Hobbies1/Content/JavaScript.js
@@ -0,0 +1,27 @@
+﻿
+alert("toto");
+
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+
+$(document).ready(function () {
+    console.log('toto');
+    toastr.info("lololololol", "lol");
+});
